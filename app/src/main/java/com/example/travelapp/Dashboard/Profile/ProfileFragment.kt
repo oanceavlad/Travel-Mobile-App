841f23e2b40a253f806f9247be22d4525b972c0f
Vlad <oancea.vlad19@yahoo.com>
2023-06-23 11:03:53 +0300

    done

--- app/src/main/java/com/example/travelapp/Dashboard/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/travelapp/Dashboard/Profile/ProfileFragment.kt
@@ -1,22 +1,27 @@
 package com.example.travelapp.Dashboard.Profile
 
-import android.content.Intent.getIntent
-import android.graphics.Color
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import com.example.travelapp.R
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.squareup.picasso.Picasso
+import java.io.File
 
 class ProfileFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,25 +29,6 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.profile_fragment, container, false)
-
-//        val user = FirebaseAuth.getInstance().currentUser
-//        val providerId = user?.providerData?.get(0)?.providerId
-//
-//        val profilePicture = view.findViewById<ImageView>(R.id.profile_picture)
-//        val emailAddress = view.findViewById<TextView>(R.id.email_address)
-//
-//        if (providerId == GoogleAuthProvider.PROVIDER_ID) {
-//            val account = GoogleSignIn.getLastSignedInAccount(requireContext())
-//            val profilePhotoUrl = account?.photoUrl
-//
-//            Picasso.get().load(profilePhotoUrl).into(profilePicture)
-//            profilePicture.setBackgroundColor(Color.WHITE)
-//
-//            emailAddress.text = GoogleSignIn.getLastSignedInAccount(view.context)?.email
-//
-//        } else if (providerId == EmailAuthProvider.PROVIDER_ID) {
-//            emailAddress.text = FirebaseAuth.getInstance().currentUser?.email
-//        }
 
         return view
     }
